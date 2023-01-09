6efefc07a03bf3a609bdc8573f42c150eef45f03
R-Lana <120555659+R-Lana@users.noreply.github.com>
2023-01-09 14:55:34 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int StartingScore = 500;
-        int refill = 1200;
+        int StartingScore = 100;
+        int refill = 1300;
         int FinalScore;
         int bonus;
+
         if (refill > 1000) {
             bonus = refill / 100;
-            FinalScore = StartingScore + refill + bonus;
         } else {
-            FinalScore = StartingScore + refill;
+
             bonus = 0;
         }
-
+        FinalScore = StartingScore + refill + bonus;
 
         System.out.print("Количество бонусных рублей - " + bonus + "; итоговый баланс - " + FinalScore + " рублей.");
 
+
     }
 }
-
